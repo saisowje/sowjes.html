03cc1b6bf9e24f9142195d51ba38a6f745e61aed
saisowje <75520548+saisowje@users.noreply.github.com>
2023-10-30 00:30:50 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,5 @@
+function changeStyle() {
+    document.getElementById("headingElement").textContent="THANKS FOR VISITING ☺"
+    document.getElementById("headingElement1").textContent="Classes start from today"
+    document.getElementById("btn").style.display="none"
+}
