95e20f6159bde57a5adcbee721bc5ef3e70d46c7
saisowje <75520548+saisowje@users.noreply.github.com>
2023-11-03 23:03:40 +0530

    Add files via upload

---
+++ 03-11task.js
@@ -0,0 +1,17 @@
+//ARRAYS
+//storing of multiple values
+
+let fruits = ["apple", "banana", "cherry"];
+let array1 = fruits.pop();
+console.log(fruits);
+console.log(array1);
+console.log( fruits[1][1] );
+console.log( fruits[1] );
+console.log( fruits[2] );
+
+let car=["BMW", "AUDI","TATA","TESLA", "KIA"];
+let array=car.pop();
+console.log(array);
+console.log(car);
+console.log(car.push("BENG"));
+console.log(car);
