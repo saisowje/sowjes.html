0239077025af9f5db852193e6c94cfdcb7eaff36
saisowje <75520548+saisowje@users.noreply.github.com>
2023-11-04 21:54:07 +0530

    Add files via upload

---
+++ 4-11.task.js
@@ -0,0 +1,25 @@
+let fruits=["apple","banana","grap","popaya"];
+console.log(fruits)
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[1][1]);
+console.log(fruits.pop());
+console.log(fruits)
+console.log(fruits.push("orange"));
+console.log(fruits);
+fruits.splice(2,0,"cherry","strabarry");
+console.log(fruits);
+fruits.splice(4,1);
+console.log(fruits);
+fruits.splice(4,1);
+console.log(fruits);
+console.log(fruits);
+console.log(fruits.reverse());
+console.log(fruits);
+console.log(fruits.shift());
+console.log(fruits.concat("lemon","almond"));
+console.log(fruits.slice(2));
+
+
+
+
