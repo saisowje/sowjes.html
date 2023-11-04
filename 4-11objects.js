784399344f04a6e03032c9a9a021c5fdd67fbaed
saisowje <75520548+saisowje@users.noreply.github.com>
2023-11-04 23:08:19 +0530

    Add files via upload

---
+++ 4-11objects.js
@@ -0,0 +1,19 @@
+//objects
+
+
+let user=["sowje", 25, "HYD","INDIA"]
+console.log(user);
+
+
+let person = {
+    name: "sowje", 
+    age: 25, 
+    city: "HYD" , 
+    country: "INDIA"
+};
+person .email= "sowje123@gmail.com";
+person.gender="female";
+console.log(person);
+console.log(person.name, person.age);
+console.log(person.name + " is " + person.age + " years old.");
+console.log(person.name +"is from " + person.city);
